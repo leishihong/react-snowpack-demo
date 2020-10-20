da0bc892b4ad0c481ee00f93c0e22fbe8faa51d2
shihong.lei@advance.ai <shihong.lei@advance.ai>
2020-10-20 17:53:58 +0800

    ✨ snowpack学习记录

--- snowpack.config.js
+++ snowpack.config.js
@@ -9,17 +9,36 @@ module.exports = {
   ],
   installOptions: {
     /* ... */
+    polyfillNode: false,
+    rollup: {
+      plugins: [require('rollup-plugin-node-polyfills')({ crypto: true })],
+    },
   },
   devOptions: {
     /* ... */
+    port: 4000,
+    // open: 'none',
   },
   buildOptions: {
     /* ... */
+    out: 'dist',
+    sourceMaps: true,
+    clean: true,
+    baseUrl: '/',
+    // metaDir: 'static',
+    // webModulesUrl: 'web',
   },
   proxy: {
     /* ... */
+    '/api': 'https://pokeapi.co/api/v2',
   },
   alias: {
     /* ... */
+    // // Type 1: Package Import Alias
+    lodash: 'lodash-es',
+    // react: 'preact/compat',
+    // // Type 2: Local Directory Import Alias (relative to cwd)
+    // components: './src/components',
+    // '@app': './src',
   },
 };
